a82b2a174da4370400239684435154b20512312a
j1nx5 <j1nx@posteo.de>
2025-12-16 11:54:28 +0100

    prepare reading multi sources

--- lib/feedreader.py
+++ lib/feedreader.py
@@ -1,9 +1,12 @@
 import feedparser
 import pandas as pd
 import json
-# from db import DBManager
-from lib.db import DBManager
+from db import DBManager
+# from lib.db import DBManager
 import logging
+import yaml
+
+
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,24 +21,49 @@ logging.basicConfig(
 
 class FeedReader:
 
-    def __init__(self, url):
-        self.__rss_url = url
+    def __init__(self):
         self.__dbm = DBManager()
+        self.__config = self._read_sources()
+
+    def _read_sources(self):
+        with open("sources.yaml", "r", encoding="utf-8") as f:
+            config = yaml.safe_load(f)
+
+        feeds = config["feeds"]
+        return feeds
+
+    # def test_yaml(self):
+    #     print(self.__config)
+
 
     def fetch_rss_feed(self):
-        raw_feed = feedparser.parse(self.__rss_url)
-        pre_data = pd.DataFrame(raw_feed.entries)
-        fetch_data = pre_data[[
-            'id',
-            'title',
-            'title_detail',
-            'link',
-            'published',
-            'author'            
-        ]]
-        for f in fetch_data.itertuples(index=False):
-            self.__dbm.insert_feed(f.id, f.title, f.title_detail.value, f.link, f.published, f.author)
+        count = 0
+        for q in self.__config:
+            count += 1
+            print(f'Runde: {count}')
+            # print(f'Quelle: {q['name']}, Url: {q['url']}')
+            '''
+            In this run:
+            q['name'] is the name of the source for table
+            q['url'] is url for the parser
+            '''
+            raw_feed = feedparser.parse(q['url'])
+            pre_data = pd.DataFrame(raw_feed.entries)
+            print(pre_data.keys())
+
+        # fetch_data = pre_data[[
+        #     'id',
+        #     'title',
+        #     'title_detail',
+        #     'link',
+        #     'published',
+        #     'author'           
+        # ]]
+        # print(fetch_data.title_detail.values)
+
+        # for f in fetch_data.itertuples(index=False):
+        #     self.__dbm.insert_feed(f.id, f.title, f.title_detail.value, f.link, f.published, f.author)
 
 if __name__ == '__main__':
-    fr = FeedReader('https://www.coindesk.com/arc/outboundfeeds/rss/')
+    fr = FeedReader()
     fr.fetch_rss_feed()
